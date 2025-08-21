096da69fe6908323ecb26605fc556f33772b4656
sebs-tech <sebastjan.rijavec@gmail.com>
2025-08-22 00:27:14 +0200

    refactor data.c

--- src/data.c
+++ src/data.c
@@ -4,10 +4,13 @@
 
 // Array of strings for the military phonetic alphabet.
 const char* PHONETIC_ALPHABET[] = {
-    "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
-    "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November",
-    "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
-    "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
+    "Alfa", "Bravo", "Charlie", "Delta", 
+		"Echo", "Foxtrot", "Golf", "Hotel", 
+		"India", "Juliett", "Kilo", "Lima", 
+		"Mike", "November", "Oscar", "Papa", 
+		"Quebec", "Romeo", "Sierra", "Tango", 
+		"Uniform", "Victor", "Whiskey", "X-ray", 
+		"Yankee", "Zulu"
 };
 
 const size_t PHONETIC_ALPHABET_SIZE = sizeof(PHONETIC_ALPHABET) / sizeof(PHONETIC_ALPHABET[0]);
